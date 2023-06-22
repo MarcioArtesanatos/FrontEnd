53bf4e3efd8f32e4c54fe4da219d8edcddd2d118
hebertdev82 <hebertdev82@gmail.com>
2023-06-22 19:02:39 -0300

    construction

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h5>Nosso site está em construção</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
