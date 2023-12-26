50179265dd7aab7335817dc7c729b3d6355e6707
Hebert <hebertdev82@gmail.com>
2023-12-26 12:13:46 -0300

    ajustes

--- src/app/pages/products/page.jsx
+++ src/app/pages/products/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import header from '/public/header_produtos.jpg';
-import Produto from "/src/app/components/product";
+import Produto from "/src/app/components/Products";
 
 export default function Produtos() {
     return (
